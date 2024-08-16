377793850774ae3b83c7da629bc6b5583183da93
michalzajac82 <59515600+michalzajac82@users.noreply.github.com>
2024-08-16 09:23:23 +0200

    4_przeplyw.js

--- 4_przeplyw.js
+++ 4_przeplyw.js
@@ -2,11 +2,15 @@ btn1.addEventListener('click', function przeplyw(){
 
   let wyd = parseFloat(document.getElementById('wyd').value);
   let dt = parseFloat(document.getElementById('dt').value);
+  let pow = parseFloat(document.getElementById('pow').value);
 
-
-  // obliczenia OZC
+  
+  // obliczenia wynik1
   let wynik1 = (wyd/(1.163*dt)).toFixed(2);
   document.querySelector('#wynik1').innerHTML = wynik1;
 
+  // obliczenia wynik2
+  let wynik2 = (wynik1*pow).toFixed(2);
+  document.querySelector('#wynik2').innerHTML = wynik2;
 
 })
